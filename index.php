339c3a847bdd55477113fee1387459f0b3bb4d9c
Atul Singh Arora <toatularora@gmail.com>
2012-08-15 18:59:29 +0530

    Made the page for the game, game logic mostly in place

--- index.php
+++ index.php
@@ -7,12 +7,70 @@
 		<script type="text/javascript" src="js/jquery-1.7.2.js"> </script>
 	</head>
 
-	<body>		
-		<h1>New Niyum</h1>		
+	<body style="background-image: url(img/pattern.png), url(img/7.jpg);">		
+		<h1>New Niyum</h1>
+		<p class="small">Disclaimer: No human was harmed during the production of this game.
+			No offence intended; the purpose of this game is not to hurt somebody, it's supposed to be entertaining, 
+			but sole entertainment is not the objective either.</p>
 		<center>
-			<div class="ga"><p>Use the arrow keys to move around <br/> Space to Jump</p></div>		
-		</center>
-		<p class="subheading">Work hard, Play harder!</p>
+			<div class="ga">
+				<p class="Status">Use the arrow keys to move around <br/> Space to Jump</p>
+
+			</div>		
+		
+			<p class="small center">Developed by</p>
+			<img src="img/theDeparted.svg" width="400px"/>		
+			<p class="small center">Nothing worth having comes without sacrifice.</p>		
+		</center>		
+		<div class="backTransparency">
+			<p>Purpose</p>
+			<p class="small">
+				The purpose is to show everyone that this place (viz. IISER M)
+				despite all its issues, does not stop us from doing amazing things
+				in life. We have some of the most rare privilege in the nation. Let's not forget
+				that the reason we have the freedom to excel, this freedom of speech, is because many struggled 
+				to achieve them for, not themselves, but us. Nothing worth having comes without sacrifice. 
+				There is always Hope. For those of us who have been burdened with too much pessimism, here 
+				is a tiny spark of hope.
+				Men and women in this nation, once decided to fight without weapons, and we ended up changing the map
+				of this world. Let us see what a set of highly trained individuals (viz. the IISER community)
+				can do.
+			</p>
+		</div>
+		<br/><br/><br/><br/><br/>
+		<div class="backTransparency">
+			<p>New Niyum</p>
+			<p class="small">
+				The game is simple. Here's a quick summary:
+				<ol>
+					<li> Keep moving with the flow </li>
+					<li> The black boxes kill you, yellows wont </li>
+					<li> Don't get caught by Dr. Urr Wind</li>
+					<li> Don't fall </li>
+					<li> Wobbly boxes will fall </li>
+					<li> Use power tiles for cool abilities 
+						<ol>
+							<li> Parity Power: Makes the Doctor run away </li>
+							<li> Shi-Fi Power: Distracts the Doctor (freezes him) </li>
+						</ol>
+					</li>
+
+				</ol>
+			</p>
+		</div>
+		<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+		<div class="backTransparency">
+			<p>About</p>
+			<p class="small">
+				Founded by Gagan Preet Singh, Atul Singh Arora and Arjit Kant Gupta, is an organization
+				that still believes:
+				<ol>
+					<li> in hard-work for the greater good </li>
+					<li> in middle names! </li>
+				</ol>
+
+			</p>
+		</div>
 		<script type="text/javascript" src="js/NewNiyum.js"> </script>
 	</body>
 
--- js/NewNiyum.js
+++ js/NewNiyum.js
@@ -1,23 +1,79 @@
 ////////////////////////////
 //ABSTRACTION ONLY PART
-///////////////////////////
+////////////////////////////
+//==================================
+//GLOBALS
+//==================================
+//---------CHARACTER------
+//CONVENTION: 
 // Position of Character,
 // frame_no 0 (standing) 1 2 3 4 (for running) 5 for jumping
 // parity, is direction of movement (1 is right movement, 0 is left)
 // footprint, the distance from x, which if in touch is considered to be in contact
-var footprint=30, x=footprint,y=0,frame_no=0,parity=1;
+var footprint=30, x=footprint,y=0,frame_no=0,parity=1,power=[{'Partha':0,'Shiv':0}];
 
-// World Position (X) and (V) is velocity in X
-var X=0,V=0;
+//---------WORLD-----
+//CONVENTION: Capitals for the world coordinates
+// Camera Position (X) and (V) is velocity in X
+var X=0,Vx=0,Y=0,Vy=0;
+var Blen=100,Bheight=10,Bricks=[{'x':0,'y':500,'color':'Yellow'},{'x':Blen,'y':500,'wobble':0,'color':'Black'},{'x':Blen*2,'y':500,'wobble':0,'color':'Yellow'},{'x':Blen*3,'y':500,'wobble':0,'color':'Yellow'}];
 
+// Projection wrt Camera
+var proj=[{'x':x,'y':y},{'Bricks':[]}];
+
+
+
+//===================================
+//FUNCTIONS
+//===================================
+//----------INIALIAZE------------------
+//To initialize all conditions that require a loop etc to execute
+function initialize(){
+	var i;
+	for(i=0;i<Bricks.len;i++)
+	{
+		proj.Bricks.append({'x':Bricks[i].Blen,'y':Bricks[i].y,'wobble':Bricks[i].wobble,'color':Bricks[i].color});	
+	}	
+}
+
+//---------------GAMES MAIN LOOP---------
+
+
+//-------------EVALUATE PROJECTION--------
+//Almost like a pre-process for render
+function evaluateProjections(){
+	proj.x=x-X;
+	proj.y=y-Y;
+	var i;
+	for(i=0;i<Bricks.len;i++)
+	{
+		proj.Bricks[i].x=Bricks[i].x-X;
+		proj.Bricks[i].y=Bricks[i].y-Y;
+	}
+}
 ////////////////////////////
 
 //////////////////////////////
 //DOM BINDING
 //////////////////////////////
+// UpdateFromAbstraction(){
+// ;
+// }
+
+function MakeObjects(){
+	var num;
+	for(num=0;num<Bricks.length;num++)
+	{
+		$('.ga').prepend('<div class="Brick" id=' + num + '> </div>');
+		$('#'+num).css('left',Bricks[num].x).css('top',Bricks[num].y).css('background-color',Bricks[num].color).css('width',Blen).css('height',Bheight);
+	}
+}
+
 $(document).ready(function(){
 ////////////DOM ADJUST///////////
 	width=$(document).width();
 	height=$(document).height();
+
+	MakeObjects();
 });
 //////////////////////////////
